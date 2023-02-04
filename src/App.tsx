c41982959c2250ca7a7511ad8b576d4d32ffa3dd
Vagrant <quazacotl@gmail.com>
2023-02-04 14:30:20 +0300

    Theme creation, css files creation.

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,20 @@
-import React, { Suspense } from 'react';
-import './index.scss'
+import React, {Suspense} from 'react';
+import '../styles/index.scss'
 import {Link, Route, Routes} from "react-router-dom";
 import {MainPageAsync} from "./pages/mailPage/MailPage.async";
 import {AboutPageAsync} from "./pages/aboutPage/AboutPage.async";
+import {useTheme} from "../theme/useTheme";
+
 
 
 const App = () => {
+    const {theme, toggleTheme} = useTheme()
+
     return (
-        <div>
+        <div className={`app ${theme}`}>
             <Link to={'/'}>Главная</Link>
             <Link to={'/about'}>О нас</Link>
+            <button onClick={toggleTheme}>TOGGLE</button>
             <Suspense fallback={<div>Loading...</div>}>
                 <Routes>
                     <Route path={'/about'} element={<AboutPageAsync/>}/>
