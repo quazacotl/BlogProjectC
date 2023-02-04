af57f5e7aa521af5d8558fc4ad5039f4ab25ff1d
Vagrant <quazacotl@gmail.com>
2023-02-04 12:12:09 +0300

    React router, lazy components.

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,23 @@
-import React from 'react';
-import {TestButton} from "./components/testButton";
+import React, { Suspense } from 'react';
 import './index.scss'
+import {Link, Route, Routes} from "react-router-dom";
+import {MainPageAsync} from "./pages/mailPage/MailPage.async";
+import {AboutPageAsync} from "./pages/aboutPage/AboutPage.async";
+
 
 const App = () => {
     return (
-        <div className={'App'}>
-            adsljfjk
-            <TestButton/>
+        <div>
+            <Link to={'/'}>Главная</Link>
+            <Link to={'/about'}>О нас</Link>
+            <Suspense fallback={<div>Loading...</div>}>
+                <Routes>
+                    <Route path={'/about'} element={<AboutPageAsync/>}/>
+                    <Route path={'/'} element={<MainPageAsync/>}/>
+                </Routes>
+            </Suspense>
         </div>
+
     );
 };
 
