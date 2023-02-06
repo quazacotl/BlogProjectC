ef3c3b476a08127488fb0e3dfa23ac58e64f8dbf
Vagrant <quazacotl@gmail.com>
2023-02-06 15:02:47 +0300

    Navbar widget creation, custom Link creation.

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,6 +1,6 @@
 import React, {Suspense} from 'react';
 import './styles/index.scss'
-import {Link, Route, Routes} from "react-router-dom";
+import {Route, Routes} from "react-router-dom";
 import {classNames} from "shared/lib/classNames/classNames";
 import {useTheme} from "app/providers/ThemeProvider";
 import {routeConfig} from "app/providers/appRouter/config/routeConfig";
@@ -9,12 +9,11 @@ import {Navbar} from "widgets/Navbar";
 
 
 const App = () => {
-    const {theme, toggleTheme} = useTheme()
+    const {theme} = useTheme()
 
     return (
         <div className={classNames('app', {}, [theme])}>
             <Navbar/>
-            <button onClick={toggleTheme}>TOGGLE</button>
             <Suspense fallback={<div>Loading...</div>}>
                 <Routes>
                     {Object.values(routeConfig).map(({path, element}) => (
--- src/widgets/Navbar/ui/Navbar.tsx
+++ src/widgets/Navbar/ui/Navbar.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import {classNames} from "shared/lib/classNames/classNames";
 import cls from './Navbar.module.scss'
 import {AppLink, AppLinkTheme} from "shared/ui/AppLink/AppLink";
+import {ThemeSwitcher} from "widgets/ThemeSwitcher";
 
 interface NavbarProps {
     className?: string
@@ -10,6 +11,7 @@ export const Navbar = (props: NavbarProps) => {
     const {className} = props
     return (
         <div className={classNames(cls.navbar, {}, [className])}>
+            <ThemeSwitcher/>
             <div className={cls.links}>
                 <AppLink theme={AppLinkTheme.SECONDARY} to={'/'}>Главная</AppLink>
                 <AppLink theme={AppLinkTheme.SECONDARY} to={'/about'}>О нас</AppLink>
