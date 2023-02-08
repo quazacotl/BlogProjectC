867651cda6f31340defbe097e103e9830ceaa7ef
vagrant <diromanov@hex.team>
2023-02-08 11:45:25 +0300

    Installed jest, wrote 1st test.

---
+++ src/shared/lib/classNames/classNames.test.ts
@@ -0,0 +1,19 @@
+import {classNames} from './classNames'
+
+describe('1st param', () => {
+	test('test', () => {
+		expect(classNames('class')).toBe('class')
+	})
+	test('with additional', () => {
+		expect(classNames('class', {}, ['additional'])).toBe('class additional')
+	})
+	test('with mods', () => {
+		expect(classNames('class', {hovered: true, scrollable: true}, ['additional'])).toBe('class additional hovered scrollable')
+	})
+	test('with false mods', () => {
+		expect(classNames('class', {hovered: true, scrollable: false}, ['additional'])).toBe('class additional hovered')
+	})
+	test('with undef mods', () => {
+		expect(classNames('class', {hovered: true, scrollable: undefined}, ['additional'])).toBe('class additional hovered')
+	})
+})
