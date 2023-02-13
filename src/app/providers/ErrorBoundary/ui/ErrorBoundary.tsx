2026570d5a4901f2644b1ce3af5bc2c29e8d24f5
vagrant <diromanov@hex.team>
2023-02-13 13:33:38 +0300

    Added error boundary.

---
+++ src/app/providers/ErrorBoundary/ui/ErrorBoundary.tsx
@@ -0,0 +1,39 @@
+import React, {ErrorInfo, ReactNode, Suspense} from 'react'
+import {PageError} from 'widgets/PageError'
+
+interface Props {
+	children: ReactNode
+}
+
+interface State {
+	hasError: boolean
+}
+
+class ErrorBoundary extends React.Component<Props, State> {
+	constructor(props: Props) {
+		super(props)
+		this.state = { hasError: false }
+	}
+
+	componentDidCatch(error: Error, errorInfo: ErrorInfo) {
+		console.log(error, errorInfo)
+	}
+
+	render() {
+		const {children} = this.props
+		const {hasError} = this.state
+
+		if (hasError) {
+			return (
+				<Suspense fallback={''}>
+					<PageError/>
+				</Suspense>
+			)
+		}
+
+		return children
+	}
+}
+
+
+export default ErrorBoundary
---
+++ src/widgets/PageError/ui/PageError.tsx
@@ -0,0 +1,27 @@
+import {FC} from 'react'
+import {classNames} from 'shared/lib/classNames/classNames'
+import cls from './PageError.module.scss'
+import {useTranslation} from 'react-i18next'
+import {Button} from 'shared/ui/Button/Button'
+
+interface PageErrorProps {
+	className?: string,
+}
+
+const reloadPage = () => {
+	location.reload()
+}
+
+export const PageError: FC<PageErrorProps> = (props) => {
+	const {className} = props
+	const {t} = useTranslation()
+
+	return (
+		<div className={classNames(cls.pageError, {}, [className]) }>
+			<p>{t('Произошла непредвиденная ошибка')}</p>
+			<Button onClick={reloadPage}>
+				{t('Обновить страницу')}
+			</Button>
+		</div>
+	)
+}
