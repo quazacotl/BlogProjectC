db846388fe264aba4aa4785e8021b292e42f3ffd
vagrant <diromanov@hex.team>
2023-02-16 13:54:48 +0300

    Fixed translations for navbar.

--- src/widgets/Navbar/ui/Navbar.tsx
+++ src/widgets/Navbar/ui/Navbar.tsx
@@ -2,17 +2,20 @@ import React from 'react'
 import {classNames} from 'shared/lib/classNames/classNames'
 import cls from './Navbar.module.scss'
 import {AppLink, AppLinkTheme} from 'shared/ui/AppLink/AppLink'
+import {useTranslation} from 'react-i18next'
 
 interface NavbarProps {
     className?: string
 }
 export const Navbar = (props: NavbarProps) => {
 	const {className} = props
+	const {t} = useTranslation()
+
 	return (
 		<div className={classNames(cls.navbar, {}, [className])}>
 			<div className={cls.links}>
-				<AppLink theme={AppLinkTheme.SECONDARY} to={'/'}>Главная</AppLink>
-				<AppLink theme={AppLinkTheme.SECONDARY} to={'/about'}>О нас</AppLink>
+				<AppLink theme={AppLinkTheme.SECONDARY} to={'/'}>{t('Главная')}</AppLink>
+				<AppLink theme={AppLinkTheme.SECONDARY} to={'/about'}>{t('О нас')}</AppLink>
 			</div>
 		</div>
 	)
