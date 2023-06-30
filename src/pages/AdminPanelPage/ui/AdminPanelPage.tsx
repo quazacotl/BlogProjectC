65ce9a11db44c29ff76cef51f7ec4610165bb8db
Vagrant <quazacotl@gmail.com>
2023-06-30 16:42:40 +0300

    LF.

--- src/pages/AdminPanelPage/ui/AdminPanelPage.tsx
+++ src/pages/AdminPanelPage/ui/AdminPanelPage.tsx
@@ -1,20 +1,20 @@
-import {classNames} from "@/shared/lib/classNames/classNames";
+import {classNames} from '@/shared/lib/classNames/classNames'
 import cls from './AdminPanelPage.module.scss'
 import {Page} from '@/widgets/Page'
 import {useTranslation} from 'react-i18next'
 
 interface AdminPanelPageProps {
-    className?: string
+	className?: string
 }
 const AdminPanelPage = (props: AdminPanelPageProps) => {
-    const {className} = props
-    const {t} = useTranslation()
+	const {className} = props
+	const {t} = useTranslation()
 
-    return (
-        <Page className={classNames(cls.adminPanelPage, {}, [className])}>
-            <h4>{t('Панель администратора')}</h4>
-        </Page>
-    );
-};
+	return (
+		<Page className={classNames(cls.adminPanelPage, {}, [className])}>
+			<h4>{t('Панель администратора')}</h4>
+		</Page>
+	)
+}
 
 export default AdminPanelPage
